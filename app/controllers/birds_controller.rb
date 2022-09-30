fe5c5e5c068e3f5171eded990863c04272fff653
SallyMulupi <mulupisally2gmail.com>
2022-09-30 23:37:08 +0300

    commit tree 906475bad358eac28f462ff2c69b9d25912ee54c

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,31 +1,34 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+  # rest of controller actio
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
 
-  # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
-
+  
+  def update
+    bird = find_bird
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird.update!(bird_params)
+    render json: bird
+  end
   # GET /birds/:id
   def show
     bird = find_bird
     render json: bird
   end
 
-  # PATCH /birds/:id
-  def update
-    bird = find_bird
-    bird.update(bird_params)
-    render json: bird
-  end
-
+  
   # DELETE /birds/:id
   def destroy
     bird = find_bird
@@ -43,8 +46,12 @@ class BirdsController < ApplicationController
     params.permit(:name, :species, :likes)
   end
 
-  def render_not_found_response
-    render json: { error: "Bird not found" }, status: :not_found
+  
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
+
+  # rest of private methods...
 
 end
